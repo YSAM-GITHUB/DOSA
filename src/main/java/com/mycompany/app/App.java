285f97753e8dac5fe5d71a30c27bfebafc1b5a0e
YSAM-GITHUB <47695729+YSAM-GITHUB@users.noreply.github.com>
2019-04-09 12:04:56 +0530

    removed 1 print

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -15,6 +15,6 @@ public class App
 		System.out.println( "Hello World!" );
 		System.out.println( "Hello World!" );
 		System.out.println( "Hello World!" );
-		System.out.println( "Hello World!" );
+		
     }
 }
