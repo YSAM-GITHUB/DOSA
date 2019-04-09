ab90408c734f10eec91e3aefc882812f97e007b7
unknown <mnys06@gmail.com>
2019-04-09 10:09:39 +0530

    my first commit

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,32 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+
+
+{
+        System.out.println( "Hello World!" );
+		System.out.println( "Hello World!" );
+		System.out.println( "Hello World!" );
+		System.out.println( "Hello World!" );
+		System.out.println( "Hello World!" );
+		System.out.println( "Hello World!" );
+		System.out.println( "Hello World!" );
+		System.out.println( "Hello World!" );
+    }
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
